5ff441cc3a564ad1ad8087e1d13b38f5ef7be4a8
Matheus Garcia Moreira <matheusgarcia37@hotmail.com>
2020-12-31 15:59:11 -0300

    css initial page-adminAcess

--- front/src/pages/adminAcess.tsx
+++ front/src/pages/adminAcess.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
 import '../styles/global.css';
+import '../styles/pages/adminAcess.css'
 
 function adminAcess(){
     return (
         <div id="page-AdminAcess">
             <div className="content-wrapper">
                 <header>
-                    <h1>Admin Acess</h1>
+                    <div className="tolbarLogin">
+                        <h1>Admin Acess</h1>
+                    </div>             
                 </header>
                 <main>
-                    <strong>
-                        <p>login:</p>
-                        <p>Senha:</p>
-                    </strong>
+                    <div className="content">
+                        <div className="formGroup-Credentials">
+                            <form action="" name="loginForm" id="login">
+                                <input type="text" name="UserName" placeholder="Seu nome de usuário" />
+                                <input type="text" name="Password" placeholder="Sua senha" />
+
+                                <input type="submit" name="send" value="enviar" />
+                            </form>
+                        </div>
+                    </div>
                 </main>
             </div>
         </div>
