1154f53108360c0dc6779e1a204391ef465181bf
Matheus Garcia Moreira <matheusgarcia37@hotmail.com>
2020-12-31 17:24:02 -0300

    quase finalizando o formulario

--- front/src/pages/adminAcess.tsx
+++ front/src/pages/adminAcess.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import '../styles/global.css';
 import '../styles/pages/adminAcess.css'
 
+import icon from '../images/icon.svg'
+import { FiArrowLeft } from 'react-icons/fi';
+
 function adminAcess(){
     return (
         <div id="page-AdminAcess">
+            <div className="spaceLogo">
+            <div className="logo">
+                <img src={icon} alt="icon" />
+                <h2>Voz do povo</h2>
+            </div>
+            </div>          
             <div className="content-wrapper">
                 <header>
                     <div className="tolbarLogin">
                         <h1>Admin Acess</h1>
+                            <Link to="/">
+                               <FiArrowLeft size={15}/> clique para voltar
+                             </Link>
                     </div>             
                 </header>
                 <main>
