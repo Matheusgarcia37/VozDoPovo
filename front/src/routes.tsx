00710ee4bc129a831d47cf7589b26f930f8cc37c
Matheus Garcia Moreira <matheusgarcia37@hotmail.com>
2021-01-03 09:49:22 -0300

    css

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import adminAcess from './pages/adminAcess';
 import denunciations from './pages/denunciations';
 import Landing from './pages/Landing';
+import generateProtocol from './pages/pages-denunciations/generateProtocol';
+import report from './pages/pages-denunciations/report';
 
 function Routes() {
     return(
@@ -11,6 +14,8 @@ function Routes() {
                 <Route path="/" exact component={Landing}/>
                 <Route path="/adminAcess" component={adminAcess} />
                 <Route path="/denunciations" component= { denunciations } />
+                <Route path="/report" component= { report } />
+                <Route path="/generateProtocol" component= { generateProtocol } />
             </Switch>
         </BrowserRouter>
     );
